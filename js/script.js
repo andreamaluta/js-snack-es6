e1918c597f39e3d643f9c00ebd817f65a4909485
andreamaluta <andreamaluta@gmail.com>
2025-03-28 13:41:28 +0100

    [UPD] created the arry of object

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const bike = [
+    {
+        bikeName: 'Trek Domane AL 3',
+        weight: 8.1,
+    },
+    {
+        bikeName: 'Cannondale CAAD Optimo Tiagra',
+        weight: 7.9,
+    },
+    {
+        bikeName: 'Giant Contend SL 2',
+        weight: 7.4,
+    },
+    {
+        bikeName: 'Scott Speedster 20',
+        weight: 7.7,
+    },
+    {
+        bikeName: 'Decathlon Triban 520',
+        weight: 8.3,
+    }
+]
+
+console.log(bike);
